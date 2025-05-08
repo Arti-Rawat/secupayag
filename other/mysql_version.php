436b6ea5f722bb4ef85b4471aa4f338dcb384459
Arti <it.artirawat@gmail.com>
2025-05-08 17:47:24 +0200

    Delete Commit

--- other/mysql_version.php
+++
@@ -1,9 +0,0 @@
-<?php
-$conn = new mysqli("localhost", "root", "");
-
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-
-echo "MySQL Server version: " . $conn->server_info;
-?>
